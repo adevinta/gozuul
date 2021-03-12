732d6487d0005af8aee2cda0b61ee30ccffc403b
Jesus Carpintero <jesus.carpintero@adevinta.com>
2021-03-12 13:37:00 +0100

    Update Open Source

--- resources/gen.go
+++ resources/gen.go
@@ -1,3 +1,7 @@
+/*
+Copyright 2021 Adevinta
+*/
+
 package resources
 
 //go:generate go run genresources.go
--- vendor/github.com/julienschmidt/httprouter/path.go
+++ vendor/github.com/julienschmidt/httprouter/path.go
@@ -1,3 +1,7 @@
+/*
+Copyright 2021 Adevinta
+*/
+
 // Copyright 2013 Julien Schmidt. All rights reserved.
 // Based on the path package, Copyright 2009 The Go Authors.
 // Use of this source code is governed by a BSD-style license that can be found
